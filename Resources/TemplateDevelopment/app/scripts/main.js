f11475853d53da7d5072d8014c9fe46b5dbbbd7f
Joekolade <joe@joekola.de>
2019-02-25 21:54:24 +0100

    Move CE nodes to own group in UI
    + some things

--- Resources/TemplateDevelopment/app/scripts/main.js
+++ Resources/TemplateDevelopment/app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+
+var Joekolade = Joekolade || {};
+
+Joekolade.preload = {
+    init: function(){
+        $('body').addClass('loading--active');
+    }
+};
+
+$(function(){
+    Joekolade.preload.init();
+});
+
+
+// if (typeof document.addEventListener === 'function') {
+//     document.addEventListener('Neos.PageLoaded', function(event) {
+//         // Do stuff
+//     }, false);
+// }
